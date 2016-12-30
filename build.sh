e58978a3cc6534f9468727a6cee0860fcddb82ba
Matthew Titmus <matt@seamlessdocs.com>
2016-12-30 18:12:33 -0500

    Fix build script

--- build.sh
+++ build.sh
@@ -6,13 +6,13 @@ GO_VERSION=1.7
 
 # Builds the binary in a go container, and drops the Linux-compatible binary in $PWD/bin
 docker run --rm \
-	-v "$PWD":/go/src/smudge \
+	-v "$PWD":/go/src/github.com/clockworksoul/smudge \
 	-v "$PWD/tmp":/go/bin \
 	-w /go/bin \
 	-e "CGO_ENABLED=0" \
 	-e "GOOS=linux" \
 	golang:${GO_VERSION} \
-	go build -a -installsuffix cgo -v smudge/smudge
+	go build -a -installsuffix cgo -v github.com/clockworksoul/smudge/smudge
 
 docker build -t clockworksoul/smudge:latest .
 
